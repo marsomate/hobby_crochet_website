f5a9a7d93eeec677f2620016a2d8e4a7c1505b43
marsomate <122260869+marsomate@users.noreply.github.com>
2023-10-30 13:08:23 +0100

    Language select 2.0

--- assets/actions.js
+++ assets/actions.js
@@ -14,17 +14,15 @@ window.onscroll = function () {
 document.addEventListener("DOMContentLoaded", function () {
     const languageSelect = document.getElementById("language-select");
     const navbarMainNavigator = document.querySelector(".navbar-main-navigator");
+    const navbarWorksNavigator = document.querySelector(".navbar-works-navigator");
+    const navbarBlogNavigator = document.querySelector(".navbar-blog-navigator");
+    const navbarContactNavigator = document.querySelector(".navbar-contact-navigator");
+    const introHeader = document.querySelector(".intro-header");
+    const introFirstP = document.querySelector(".intro-first-p");
+    const introSecondP = document.querySelector(".intro-second-p");
+    const introThirdP = document.querySelector(".intro-third-p");
 
     const selectedLanguage = getLanguage();
-
-    // Alapértelmezett nyelv
-    if (selectedLanguage === "hu") {
-        navbarMainNavigator.textContent = "Főoldal";
-        navbarMainNavigator.classList.add("nav-item")
-    } else if (selectedLanguage === "en") {
-        navbarMainNavigator.textContent = "Home";
-        navbarMainNavigator.classList.add("nav-item")
-    }
 
     // Nyelvválasztás eseménykezelő
     languageSelect.addEventListener("change", function () {
@@ -33,17 +31,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (selectedLanguage === "hu") {
             navbarMainNavigator.textContent = "Főoldal";
-            navbarMainNavigator.classList.add("nav-item")
+            navbarWorksNavigator.textContent = "Művek";
+            navbarBlogNavigator.textContent = "Blog";
+            navbarContactNavigator.textContent = "Elérhetőség";
+            introHeader.textContent = "Bemutatkozás";
+            introFirstP.textContent = "HULorem ipsum, dolor sit amet consectetur adipisicing elit. Earum laboriosam fugit consequatur ducimus. Eius sed libero, harum porro natus autem eaque dolor nobis sunt, error, nesciunt temporibus culpa illo tempore!";
+            introSecondP.textContent = "HULorem ipsum, dolor sit amet consectetur adipisicing elit. Earum laboriosam fugit consequatur ducimus. Eius sed libero, harum porro natus autem eaque dolor nobis sunt, error, nesciunt temporibus culpa illo tempore!";
+            introThirdP.textContent = "HULorem ipsum, dolor sit amet consectetur adipisicing elit. Earum laboriosam fugit consequatur ducimus. Eius sed libero, harum porro natus autem eaque dolor nobis sunt, error, nesciunt temporibus culpa illo tempore!";
+
         } else if (selectedLanguage === "en") {
             navbarMainNavigator.textContent = "Home";
-            navbarMainNavigator.classList.add("nav-item")
+            navbarWorksNavigator.textContent = "Works";
+            navbarBlogNavigator.textContent = "Blog";
+            navbarContactNavigator.textContent = "Contact";
+            introHeader.textContent = "Itroduction";
+            introFirstP.textContent = "ENGLorem ipsum, dolor sit amet consectetur adipisicing elit. Earum laboriosam fugit consequatur ducimus. Eius sed libero, harum porro natus autem eaque dolor nobis sunt, error, nesciunt temporibus culpa illo tempore!";
+            introSecondP.textContent = "ENGLorem ipsum, dolor sit amet consectetur adipisicing elit. Earum laboriosam fugit consequatur ducimus. Eius sed libero, harum porro natus autem eaque dolor nobis sunt, error, nesciunt temporibus culpa illo tempore!";
+            introThirdP.textContent = "ENGLorem ipsum, dolor sit amet consectetur adipisicing elit. Earum laboriosam fugit consequatur ducimus. Eius sed libero, harum porro natus autem eaque dolor nobis sunt, error, nesciunt temporibus culpa illo tempore!";
+
         }
     });
 });
 
 // Cookie kezelése
 function setLanguage(lang) {
-    document.cookie = "language=" + lang;
+    document.cookie = `language=${lang}; path=/`; // A "path=/" beállítja az útvonalat, hogy az összes oldalon elérhető legyen
 }
 
 function getLanguage() {
